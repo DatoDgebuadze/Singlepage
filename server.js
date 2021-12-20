be2c32d300edb7ab132ec209158b73d3f641467f
DatoDgebuadze <davit.dgebuadze.1@btu.edu.ge>
2021-12-20 20:25:01 +0400

    Trying to build up the server properly

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.get("/* ", (req, res) => {
-    res.sendFile(path.resolve("Frontend", "index.html"));
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("serveri chairto Dgebu"));
+app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
